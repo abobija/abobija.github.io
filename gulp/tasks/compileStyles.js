7c0a406127b29e20e80b8fa79692d695f058911c
Alija Bobija <abobijax@gmail.com>
2020-11-26 21:46:24 +0100

    While we use @import there is no need for concat

--- gulp/tasks/compileStyles.js
+++ gulp/tasks/compileStyles.js
@@ -1,6 +1,6 @@
 const { OUT_DIR, DEV, envp } = require('./../constants');
 const gulp = require('gulp');
-const concat = require('gulp-concat');
+/*const concat = require('gulp-concat');*/
 const { sassCompiler } = require('./../extensions');
 const sourcemaps = require('gulp-sourcemaps');
 const postcss = require('gulp-postcss');
@@ -12,7 +12,7 @@ module.exports = function compileStyles() {
             'src/styles/**/*.scss'
         ])
         .pipe(envp(DEV, sourcemaps.init()))
-            .pipe(concat('style.scss'))
+            /*.pipe(concat('style.scss'))*/
             .pipe(sassCompiler())
             .pipe(postcss([ autoprefixer, cssnano ]))
         .pipe(envp(DEV, sourcemaps.write('.')))
